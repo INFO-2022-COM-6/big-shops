2141d48731ac95e9c16931b9d3054d0b180dea96
Exebrian18 <81400501+Exebrian18@users.noreply.github.com>
2022-07-02 12:43:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/")  #inicio de ruta de inicio
 def index():
 	return render_template("index.html")
 
-@app.route("/nosotros")
- def nosotros():
-    return render_template("nosotros.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
